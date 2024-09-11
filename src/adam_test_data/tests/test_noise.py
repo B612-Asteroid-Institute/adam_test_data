289319835416c4d5212d25ffab5259728bb0c212
Joachim Moeyens <moeyensj@gmail.com>
2024-09-11 11:55:15 -0700

    Use Cartesian coordinates to determine if points fall within a circle on a unit sphere

--- src/adam_test_data/tests/test_noise.py
+++ src/adam_test_data/tests/test_noise.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from ..noise import fix_wrap_around, magnitude_model
+from ..noise import fix_wrap_around, identify_within_circle, magnitude_model
 
 
 def test_magnitude_model():
@@ -21,3 +21,50 @@ def test_fix_wrap_around():
     ra = np.array([0, 360, 361, -1, -2])
     ra_expected = np.array([0, 0, 1, 359, 358])
     np.testing.assert_array_equal(fix_wrap_around(ra), ra_expected)
+
+
+def test_identify_within_circle():
+    # Test that identify within circle can correctly handle a simple circle near the equator
+    ra = np.array([30.0, 34.0, 32.0, 32.0, 32.0])
+    dec = np.array([0.0, 0.0, 2.0, -2.0, 0.0])
+
+    ra_center = 32.0
+    dec_center = 0.0
+
+    radius = 1.01
+    mask = identify_within_circle(ra, dec, ra_center, dec_center, radius)
+    expected_mask = np.array([False, False, False, False, True])
+    np.testing.assert_array_equal(mask, expected_mask)
+
+    radius = 2.01
+    mask = identify_within_circle(ra, dec, ra_center, dec_center, radius)
+    expected_mask = np.array([True, True, True, True, True])
+    np.testing.assert_array_equal(mask, expected_mask)
+
+    # Test wrap arounds past 360 degrees of RA
+    ra = np.array([359.0, 1.0])
+    dec = np.array([0.0, 0.0])
+    ra_center = 0.0
+    dec_center = 0.0
+
+    radius = 1.01
+    mask = identify_within_circle(ra, dec, ra_center, dec_center, radius)
+
+    expected_mask = np.array([True, True])
+    np.testing.assert_array_equal(mask, expected_mask)
+
+    # Test wrap arounds near the poles
+    # RA spans 0 to 360 here
+    ra = np.array([0.0, 90.0, 180.0, 270.0, 360.0])
+    dec = np.array([89.0, 89.0, 89.0, 89.0, 89.0])
+    ra_center = 0.0
+    dec_center = 90.0
+
+    radius = 1.01
+    mask = identify_within_circle(ra, dec, ra_center, dec_center, radius)
+    expected_mask = np.array([True, True, True, True, True])
+    np.testing.assert_array_equal(mask, expected_mask)
+
+    mask = identify_within_circle(ra, -dec, ra_center, dec_center, radius)
+    expected_mask = np.array([False, False, False, False, False])
+    np.testing.assert_array_equal(mask, expected_mask)
